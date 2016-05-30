708c867ce647e5bedb1094532b85a051555b8f7b
Vad-mik <vad.mikhailov1@gmail.com>
2016-05-30 18:15:04 +0400

    added main (empty so far) interfaces and classes

---
+++ mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Model/IModel.java
@@ -0,0 +1,7 @@
+package com.proba.statperson.Model;
+
+/**
+ * Created by vadik on 30.05.2016.
+ */
+public interface IModel {
+}
---
+++ mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Presenter/PresenterImpl.java
@@ -0,0 +1,7 @@
+package com.proba.statperson.Presenter;
+
+/**
+ * Created by vadik on 30.05.2016.
+ */
+public class PresenterImpl {
+}
