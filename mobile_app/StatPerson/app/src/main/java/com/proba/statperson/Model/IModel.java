f61ab4ae79d786ce2517c46e219fc69f7b057c3a
Vad-mik <vad.mikhailov1@gmail.com>
2016-05-30 18:48:46 +0400

    added some common methods

--- mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Model/IModel.java
+++ mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Model/IModel.java
@@ -4,4 +4,5 @@ package com.proba.statperson.Model;
  * Created by vadik on 30.05.2016.
  */
 public interface IModel {
+    void adminGetListOfCatalogElements(int catalogIndex);
 }
--- mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Presenter/IPresenter.java
+++ mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Presenter/IPresenter.java
@@ -1,7 +1,18 @@
 package com.proba.statperson.Presenter;
 
+import com.proba.statperson.Presenter.CatalogElement.CatalogElement;
+
 /**
  * Created by vadik on 30.05.2016.
  */
 public interface IPresenter {
+    void adminGetListOfCatalogElements(int catalogIndex);
+
+    void adminAddElement(String[] elementParameters);
+
+    void adminDeleteElement(int elementIndex);
+
+    void adminEditElement(int elementIndex);
+
+    void adminOnRetrieveListOfCatalogElements (CatalogElement[] elements);
 }
