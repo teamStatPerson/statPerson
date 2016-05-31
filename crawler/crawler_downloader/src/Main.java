ee42cfc54b8abe0df244448ad2afad022a365f3d
TurubchukAndrey <antur1977@rambler.ru>
2016-05-31 22:44:34 +0300

    1. Исппавлены неточности (вместо qz написано gz). 2. Если нет файла sitemap.xml или нет файла sitemap.xml - пустой, то в список url записывается новая страница. 3. Список URL - результат работы метода  doParseXML класса ParsingXML

--- crawler/crawler_downloader/src/Main.java
+++ crawler/crawler_downloader/src/Main.java
@@ -3,6 +3,7 @@
  */
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.regex.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,9 +19,21 @@ public class Main {
             System.out.println(URLSiteMap);*/
             //ParsingXML parsingXML = new ParsingXML("C://temp/sitemap7/sitemap7.xml");
            // String s1 = "http://www.news29.ru/sitemap.xml";
-            String s2 = "https://lenta.ru/sitemap.xml";
+            String s3 = "http://www.statperson.webtm.ru";
+            String s2 = "https://lenta.ru";
             ParsingXML parsingXML = new ParsingXML(s2);
-          //  parsingXML.printUrlTotal();
+
+            List urlSites = parsingXML.doParseXML();
+            printUrl(urlSites);
+
+
             //parsingXML.parseXML();
         }
+    public static void printUrl(List<String> listUrl) {
+        for (String url : listUrl) {
+            System.out.println("url= " + url);
+        }
+        System.out.println("Количество найденных url = " + listUrl.size() );
+    }
+
 }
