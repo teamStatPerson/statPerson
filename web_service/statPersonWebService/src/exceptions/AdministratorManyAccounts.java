186a88fe6c112a8a4666776e98adad14b41e3b19
konstantin8105 <konstantin8105@gmail.com>
2016-06-04 12:41:16 +0300

    Testing

--- web_service/statPersonWebService/src/exceptions/AdministratorManyAccounts.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class AdministratorManyAccounts extends Exception {
-	private static final long serialVersionUID = 1L;
-	public AdministratorManyAccounts(){
-		super();
-	}
-}
