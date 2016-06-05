4a1e1b5f88d63091a04883af8baa30af182679d6
konstantin8105 <konstantin8105@gmail.com>
2016-06-06 02:11:25 +0300

    interfaceAPI ... done

--- web_service/statPersonWebService/src/statPerson/api/interfaceAPI.java
+++ web_service/statPersonWebService/src/statPerson/api/interfaceAPI.java
@@ -1,43 +1,108 @@
 package statPerson.api;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import statPerson.element.account.Account;
+import statPerson.element.administrator_price.AdministratorPrice;
 import statPerson.element.keyword.Keyword;
 import statPerson.element.page.Page;
 import statPerson.element.person.Person;
+import statPerson.element.price.Price;
 import statPerson.element.site.Site;
 
 public interface interfaceAPI{
 
-	//ADMINISTRATOR
+	/**********************************************/
 	// Only primary administrator can pay for price
 	Integer addPrimaryAdministrator(String email, String password);
-	
-	//par.3.1.1. Primary administrator can create secondary administrator
+
+	// par.3.1.1. Primary administrator can create secondary administrator
 	Integer addSecondaryAdministrator(String email, String password, int idLinkedAdministrator);
-	
-	//par.3.2.1. Administrator can create and delete user 
+
+	// par.3.2.1. Administrator can create and delete user 
 	Integer addUser(String email, String password, int idLinkedAdministrator);
 	void removeAccount(Integer idAccount);
-	
-	//par.3.2.2.
+
+	// par.3.2.2.
 	List<Person> getPersons(Account account);
-	
-	//par.3.3.2.
+
+	// par.3.3.2.
 	void changePassword(Account account, String newPassword);
 	
+	/**********************************************/
+	// par.4.1.1
+	// Sites
+	List<Site> getSites(Account administrator);
 	
+	//return id of site
+	Integer addSite(Account administrator, Site site);
 	
+	void removeSite(Account administrator, Site site);
+
+	// par.4.1.2
+	// Persons
 	
+	// return id of person
+	Integer addPerson(Account administrator, Person person);
 	
-	// Administrator statistic
+	void removePerson(Account administrator, Person person);
+
+	/**********************************************/
+	
+	// par.4.1.3
+	// Prices
+	// return all allowable prices
+	List<Price> getPrices();
+	
+	// Only Primary administrator can buy the price
+	void addPriceToPrimaryAdministrator(Integer idAccount, Integer idPrice);
+	
+	// return all price of that administrator
+	List<AdministratorPrice> getAllPricesAdministrator(Integer idAdministrator);
+
+	/**********************************************/
+	
+	// ADMINISTRATOR STATISTIC
 	// par.5.1.1
 	int amountPagesFromSite(Site site);
+	
 	// par.5.1.2
 	int amountPagesInDBbutNeverScan(Site site);
+	
 	// par.5.1.3
 	int amountPagesInDBscanned(Site site);
+	
+	/**********************************************/
+	
+	// USER STATISTIC
+	// par.6.1.1 ДТЗ
+	// par.2.1.1 Функциональные требования
+	Map<Date, Integer> getRankByPerDayByPerson(Person person, Date beginDate, Date endDate);
+	
+	// par.6.1.2
+	Map<Person, Page> getStatisticPersonPage();
+	
+	// par.6.1.3
+	Map<Person, Page> getStatisticPersonNewPages();
+	
+	// par.6.2.2
+	Map<Site, Integer> getStatisticSiteAmountPage();
+	
+	// par.6.4
+	// ignored
+	
+	/**********************************************/
+	
+	// par.7.1
+	// ignored
+	
+	// par.7.2
+	// ignored
+	
+	/**********************************************/
+	
 	
 
 	//Pages for krauler statistic
@@ -50,17 +115,6 @@ public interface interfaceAPI{
 	Account getAdministrator(String email, String password);
 
 	Account getUser(String email, String password);
-	
-	// Persons
-	// return null, if haven`t persons
-	void addPerson(Account administrator, Person person);
-	void removePerson(Account administrator, Person person);
-	
-	// Sites
-	// return null, if haven`t sites
-	List<Site> getSites(Account administrator);
-	void addSite(Account administrator, Site site);
-	void removeSite(Account administrator, Site site);
 	
 	
 	// Keywords
