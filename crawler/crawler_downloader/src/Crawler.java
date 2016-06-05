6064d6734a114e4315299a992cc8ee887d6bc9b2
TurubchukAndrey <antur1977@rambler.ru>
2016-06-05 23:15:51 +0300

    Создан класс Crawler

---
+++ crawler/crawler_downloader/src/Crawler.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Андрей on 05.06.2016.
+ */
+public class Crawler {
+    private List<String> urlSites = new ArrayList<String>();
+
+    public Crawler() {
+    }
+
+    public void doJob(String site, List<String > keywords){
+        ParsingRobots parsingRobots = new ParsingRobots(site);
+        //parsingRobots.getRobotsFileFound();
+        if (parsingRobots.getURLsiteMapFound() == true) { // если ссылка на sitemap найдена в robots.txt
+            doParsing(site, parsingRobots.getUrlSiteMap());
+        } else {
+            doParsing(site, site +"/sitemap.xml");
+        }
+        printUrl(urlSites);
+
+        doStatPerson(keywords);
+    }
+//raitingPutin - рейтинг персоны на каждой странице сайта
+//raitngTotal - рейтинг персоны на всем сайте
+
+    private void doStatPerson(List<String> keywords) {
+        Integer raitngTotal = 0;
+        for (String url : urlSites) {
+            ParsingHTML parsingHTML = new ParsingHTML(url, keywords);
+
+            int raitingPutin = parsingHTML.raitingPerson();
+            raitngTotal = raitngTotal + raitingPutin;
+            // TODO: 05.06.2016 сюда вставить запись в таблицу PagePersonRank рейтинг личности, url - имя сайта, raitingPutin - рейтинг на странице url)
+            System.out.println("На странице " + url + " Рейтинг равен " + raitingPutin);
+        }
+        System.out.println("Общий рейтинг " + raitngTotal);
+    }
+
+    private void doParsing(String siteName, String urlSiteMap){
+        //List<String> urlSites = new ArrayList<String>();
+        ParsingXML parsingXML = new ParsingXML(siteName, urlSiteMap);
+        urlSites = parsingXML.getListUrl();
+        // TODO: 05.06.2016 сюда вставить запись в таблицу Pages сайта (значение переменной urlSites)
+    }
+
+    public static void printUrl(List<String> listUrl) {
+        /*for (String url : listUrl) {
+            System.out.println("url= " + url);
+        }*/
+        System.out.println("Количество найденных url = " + listUrl.size() );
+    }
+
+
+}
+
+
+
