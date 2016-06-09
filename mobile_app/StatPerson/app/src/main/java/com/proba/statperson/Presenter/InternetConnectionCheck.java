15e21799b392a8cf74d6b507b1521c2e7a2e7be1
Bayr.Tserenov <cer-bair@yandex.ru>
2016-06-09 23:55:19 +0300

    создан класс для проверки интернет соеднинения

--- mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Presenter/InternetConnectionCheck.java
+++ mobile_app/StatPerson/app/src/main/java/com/proba/statperson/Presenter/InternetConnectionCheck.java
@@ -1,7 +1,20 @@
 package com.proba.statperson.Presenter;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 /**
  * Created by SAMSUNG on 07.06.2016.
  */
 public class InternetConnectionCheck {
+
+    public boolean isInternet(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        if ((networkInfo!=null)&&(networkInfo.isConnected())){
+            return true;
+        }else return false;
+    }
+
 }
