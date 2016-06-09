2eb714fa3e711c511a9497e85c0e10cce9749ff5
Konstantin Konstantinov <keek@mail2k.ru>
2016-06-09 22:12:17 +0300

    GraphView example

--- mobile_app/StatPerson/app/src/main/java/com/proba/statperson/view/user/TotalStatGraphView.java
+++ mobile_app/StatPerson/app/src/main/java/com/proba/statperson/view/user/TotalStatGraphView.java
@@ -1,7 +1,30 @@
 package com.proba.statperson.view.user;
 
-/**
- * Created by Konstantin on 09.06.2016.
- */
-public class TotalStatGraphView {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.jjoe64.graphview.GraphView;
+import com.proba.statperson.R;
+
+public class TotalStatGraphView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_total_stat_graph_view);
+
+        // Линейный график
+        GraphViewSeries exampleSeries = new GraphViewSeries(
+                new GraphViewData[]{new GraphViewData(1, 3.0d),
+                        new GraphViewData(2, 1.5d), new GraphViewData(3, 2.5d),
+                        new GraphViewData(4, 1.0d), new GraphViewData(5, 1.3d)});
+
+        GraphView graphView = new LineGraphView(this, "График каких-то данных");
+        graphView.addSeries(exampleSeries);
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.total_stat_graph_view);
+        layout.addView(graphView);
+
+    }
 }
