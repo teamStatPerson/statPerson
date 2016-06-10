36486db60ac240e66b59006b79fbfcb2b0d8e7e0
Konstantin Konstantinov <keek@mail2k.ru>
2016-06-10 07:20:28 +0300

    GraphView example 2

--- mobile_app/StatPerson/app/src/main/java/com/proba/statperson/view/user/TotalStatGraphView.java
+++ mobile_app/StatPerson/app/src/main/java/com/proba/statperson/view/user/TotalStatGraphView.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 import com.proba.statperson.R;
 
 public class TotalStatGraphView extends AppCompatActivity {
@@ -16,12 +17,22 @@ public class TotalStatGraphView extends AppCompatActivity {
         setContentView(R.layout.activity_total_stat_graph_view);
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
+        graph.setTitle(getResources().getString(R.string.total_stat));
+        graph.setTitleTextSize(getResources().getDimension(R.dimen.head_text));
+        graph.setTitleColor(getResources().getColor(R.color.colorAccent));
+
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[] {
+        getResources().getString(R.string.putin),
+        getResources().getString(R.string.medvedev),
+        getResources().getString(R.string.navalny),
+        });
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
+
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 6),
                 new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(4, 1)
         });
         graph.addSeries(series);
 
